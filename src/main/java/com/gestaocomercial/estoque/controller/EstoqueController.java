09208f65b9b5911515d800512ce1d7c3664e0b79
Erick <erick.javaspringcount@gmail.com>
2026-09-20 00:30:04 -0300

    Adiciona service e endpoint para listar movimentacoes

--- src/main/java/com/gestaocomercial/estoque/controller/EstoqueController.java
+++ src/main/java/com/gestaocomercial/estoque/controller/EstoqueController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/estoque")
@@ -23,6 +25,11 @@ public class EstoqueController {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(estoqueService.registrarMovimentacao(movimentacaoEstoqueDTORequest));
 
+    }
+
+    @GetMapping("/movimentacao")
+    public ResponseEntity<List<MovimentacaoEstoqueDTOResponse>> listarMovimentacoes(){
+        return ResponseEntity.ok(estoqueService.listarMovimentacoes());
     }
 
 }
